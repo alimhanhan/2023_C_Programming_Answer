2561d47a09525da8ddf5ff37e232268d9f23446a
Leona <102565567+alimhanhan@users.noreply.github.com>
2023-01-11 12:18:31 +0900

    Add files via upload

---
+++ 누구나 쉽게 즐기는 C 언어 콘서트_2장 Lab 기본/누구나 쉽게 즐기는 C 언어 콘서트_2장 Lab 기본/72p_Lab 기본.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+	int first = 0;
+	int second = 0;
+
+	printf("첫 번째 숫자를 입력하시오: ");
+	scanf_s("%d", &first);
+
+	printf("두 번째 숫자를 입력하시오: ");
+	scanf_s("%d", &second);
+
+	int sum = first + second;
+	int min = first - second;
+	int mul = first * second;
+	int div = first / second;
+
+	printf("두 수의 합: %d \n", sum);
+	printf("두 수의 차: %d \n", min);
+	printf("두 수의 곱: %d \n", mul);
+	printf("두 수의 몫: %d", div);
+
+	return 0;
+}
